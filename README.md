c44113d25b5c76e02c16ab73ec4a8f25581cb129
al-adhamal-qsous <adham.goussous@gmail.com>
2017-05-11 11:19:30 +0300

    add templates to verification and restPassword

--- README.md
+++ README.md
@@ -27,6 +27,39 @@ const server = new ParseServer({
   emailAdapter: PostmarkAdapter({
     apiKey: 'your-postmark-server-api-token',
     fromAddress: 'your-email@postmark-sender-signatures.com',
+    // Verification email subject
+    verificationSubject: 'Please verify your e-mail for *|appname|*',
+    // Verification email body. This will be ignored when verificationTemplateName is used.
+    verificationBody: 'Hi *|username|*,\n\nYou are being asked to confirm the e-mail address *|email|* with *|appname|*\n\nClick here to confirm it:\n*|link|*',
+    // Password reset email subject
+    passwordResetSubject: 'Password Reset Request for *|appname|*',
+    // Password reset email body. This will be ignored when passwordResetTemplateName is used.
+    passwordResetBody: 'Hi *|username|*,\n\nYou requested a password reset for *|appname|*.\n\nClick here to reset it:\n*|link|*',
+
+    /****************************************
+    * If you are using Postmark templates: *
+    ****************************************/
+
+    //
+    // If you want to use other custom User attributes in the emails
+    // (for example: firstName, lastName), add them to the list (username and email 
+    // are pre-loaded).
+    // The merge tag in the template must be equal to the attribute's name.
+    customUserAttributesMergeTags: ['firstname', 'lastname'],
+
+    //
+    // The name of your Mandrill template for the password reset email:
+    // If you add this attribute, then passwordResetBody will be ignored.
+    // IMPORTANT: Make sure the email has the *|link|* merge tag,
+    //            it will render the url to reset the password.
+    passwordResetTemplateId: 'password-reset-template-name',
+
+    //
+    // The name of your Mandrill template for the verification email:
+    // If you add this attribute, then verificationBody will be ignored.
+    // IMPORTANT: Make sure the email has the *|link|* merge tag,
+    //            it will render the url to verify the user.
+    verificationTemplateId: 'email-verification-template-name',
   }),
 });
 
--- src/index.js
+++ src/index.js
@@ -1,33 +1,165 @@
-import { Client } from 'postmark';
+import { Postmark } from 'postmark';
 
-const PostmarkAdapter = ({ apiKey, fromAddress }) => {
-  if (!apiKey) {
-    throw new Error('PostmarkAdapter requires an API Key. You might obtain an API Token from your Postmark account settings.'); // eslint-disable-line max-len
-  } else if (!fromAddress) {
-    throw new Error('PostmarkAdapter requires fromAddress.');
+const PostmarkAdapter = postmarkOptions => {
+
+  if (
+    !postmarkOptions ||
+    !postmarkOptions.apiKey ||
+    !postmarkOptions.fromAddress
+  ) {
+    throw 'PostmarkAdapter requires an API Key and a From Email Address.';
   }
 
-  const client = new Client(apiKey);
+  postmarkOptions.replyTo =
+    postmarkOptions.replyTo ||
+    postmarkOptions.fromAddress;
+  postmarkOptions.displayName =
+    postmarkOptions.displayName ||
+    postmarkOptions.replyTo;
+  postmarkOptions.verificationSubject =
+    postmarkOptions.verificationSubject ||
+    'Please verify your e-mail for *|appname|*';
+  postmarkOptions.verificationBody =
+    postmarkOptions.verificationBody ||
+    'Hi,\n\nYou are being asked to confirm the e-mail address *|email|* ' +
+    'with *|appname|*\n\nClick here to confirm it:\n*|link|*';
+  postmarkOptions.passwordResetSubject =
+    postmarkOptions.passwordResetSubject ||
+    'Password Reset Request for *|appname|*';
+  postmarkOptions.passwordResetBody =
+    postmarkOptions.passwordResetBody ||
+    'Hi,\n\nYou requested a password reset for *|appname|*.\n\nClick here ' +
+    'to reset it:\n*|link|*';
+  postmarkOptions.customUserAttributesMergeTags = postmarkOptions.customUserAttributesMergeTags || [];
 
-  const sendMail = ({ to, subject, text }) => new Promise((resolve, reject) => {
-    client.sendEmail({
-      From: fromAddress,
-      To: to,
-      Subject: subject,
-      TextBody: text,
-    }, (err, body) => {
-      if (err) {
-        // eslint-disable-next-line no-console
-        console.error(`Unable to send via postmark: ${err.message}`);
-        reject(err);
+  const postmarkClient = new Postmark(apiKey);
+
+  var sendVerificationEmail = options => {
+    var messageData = {
+      appname: options.appName,
+      username: options.user.get("username"),
+      email: options.user.get("email"),
+      link: options.link
+    }
+
+    if (typeof postmarkOptions.customUserAttributesMergeTags !== 'undefined') {
+      for (var extra_attr of postmarkOptions.customUserAttributesMergeTags) {
+        messageData[extra_attr] = options.user.get(extra_attr) || '';
       }
-      resolve(body);
+    }
+
+    return new Promise((resolve, reject) => {
+      if (postmarkOptions.verificationTemplateId) {
+        var message = {
+          From: postmarkOptions.fromAddress,
+          To: options.user.get("email") || options.user.get("username"),
+          Subject: postmarkOptions.verificationSubject,
+          TemplateId: postmarkOptions.verificationTemplateId,
+          TemplateModel: messageData
+        }
+        postmarkClient.sendEmailWithTemplate(message,
+          function (error, result) {
+            if (error) {
+              console.error("Unable to send via postmark: " + error.message);
+              return;
+            }
+            console.info("Sent to postmark for delivery")
+          });
+      } else {
+        var message = {
+          From: postmarkOptions.fromAddress,
+          To: options.to,
+          Subject: options.subject,
+          TextBody: options.text
+        }
+        postmarkClient.sendEmail(message,
+          function (error, result) {
+            if (error) {
+              console.error("Unable to send via postmark: " + error.message);
+              return;
+            }
+            console.info("Sent to postmark for delivery")
+          });
+      }
     });
-  });
+  }
+
+  var sendPasswordResetEmail = options => {
+
+    var messageData = {
+      appname: options.appName,
+      username: options.user.get("username"),
+      email: options.user.get("email"),
+      link: options.link
+    }
+
+    if (typeof postmarkOptions.customUserAttributesMergeTags !== 'undefined') {
+      for (var extra_attr of postmarkOptions.customUserAttributesMergeTags) {
+        messageData[extra_attr] = options.user.get(extra_attr) || '';
+      }
+    }
+
+    return new Promise((resolve, reject) => {
+      if (postmarkOptions.passwordResetTemplateId) {
+        var message = {
+          From: postmarkOptions.fromAddress,
+          To: options.user.get("email") || options.user.get("username"),
+          Subject: postmarkOptions.passwordResetSubject,
+          TemplateId: postmarkOptions.passwordResetTemplateId,
+          TemplateModel: messageData
+        }
+        postmarkClient.sendEmailWithTemplate(message,
+          function (error, result) {
+            if (error) {
+              console.error("Unable to send via postmark: " + error.message);
+              return;
+            }
+            console.info("Sent to postmark for delivery")
+          });
+      } else {
+        var message = {
+          From: postmarkOptions.fromAddress,
+          To: options.to,
+          Subject: options.subject,
+          TextBody: options.text
+        }
+        postmarkClient.sendEmail(message,
+          function (error, result) {
+            if (error) {
+              console.error("Unable to send via postmark: " + error.message);
+              return;
+            }
+            console.info("Sent to postmark for delivery")
+          });
+      }
+    });
+  }
+
+  var sendMail = options => {
+    var message = {
+      From: postmarkOptions.fromAddress,
+      To: options.to,
+      Subject: options.subject,
+      TextBody: options.text
+    }
+
+    return new Promise((resolve, reject) => {
+      postmarkClient.sendEmail(message,
+        function (error, result) {
+          if (error) {
+            console.error("Unable to send via postmark: " + error.message);
+            return;
+          }
+          console.info("Sent to postmark for delivery")
+        });
+    });
+  }
 
   return Object.freeze({
-    sendMail,
+    sendVerificationEmail: sendVerificationEmail,
+    sendPasswordResetEmail: sendPasswordResetEmail,
+    sendMail: sendMail
   });
-};
+}
 
 module.exports = PostmarkAdapter;
